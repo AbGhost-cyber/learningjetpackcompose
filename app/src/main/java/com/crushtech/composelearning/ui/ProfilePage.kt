b034399e75ff9d9752afab8e83c83cfda335c740
abGhost.cyber <xplendo@gmail.com>
2022-04-17 14:20:08 +0800

    jetpack compose basics workshop

--- app/src/main/java/com/crushtech/composelearning/ui/ProfilePage.kt
+++ app/src/main/java/com/crushtech/composelearning/ui/ProfilePage.kt
@@ -3,10 +3,8 @@ package com.crushtech.composelearning.ui
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Text
@@ -19,6 +17,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import com.crushtech.composelearning.R
 
 @Composable
@@ -30,11 +30,9 @@ fun ProfilePage() {
             .padding(top = 100.dp, start = 16.dp, end = 16.dp, bottom = 100.dp)
             .border(width = 2.dp, color = Color.White, shape = RoundedCornerShape(30.dp))
     ) {
-        Column(
-            Modifier.verticalScroll(rememberScrollState()),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
+        ConstraintLayout {
+            val guideLine = createGuidelineFromTop(0.1f)
+            val (image, nameText, subText, rowStats, btnFollow, btnMsg) = createRefs()
             Image(
                 painter = painterResource(id = R.drawable.kermit_the_frog),
                 contentDescription = "Kermit the frog",
@@ -44,37 +42,67 @@ fun ProfilePage() {
                     .border(
                         width = 3.dp,
                         color = Color.Red, shape = CircleShape
-                    ),
+                    )
+                    .constrainAs(image) {
+                        top.linkTo(guideLine)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
             )
-            Text(text = "German Kermit", fontWeight = FontWeight.ExtraBold)
-            Text(text = "hello, welcome to my page")
-            ProfileRow {
+            Text(
+                text = "German Kermit", fontWeight = FontWeight.ExtraBold,
+                modifier = Modifier.constrainAs(nameText) {
+                    top.linkTo(image.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+            )
+            Text(
+                text = "hello, welcome to my page",
+                modifier = Modifier.constrainAs(subText) {
+                    top.linkTo(nameText.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+            )
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .constrainAs(rowStats) {
+                        top.linkTo(subText.bottom)
+                    }
+            ) {
                 ProfileStats(count = 15000, desc = "Followers")
                 ProfileStats(count = 100, desc = "Following")
                 ProfileStats(count = 13, desc = "Post")
                 ProfileStats(count = 300, desc = "Saved")
             }
-            ProfileRow {
-                Button(onClick = {}) {
-                    Text(text = "Follow user")
+
+            Button(
+                onClick = {},
+                modifier = Modifier.constrainAs(btnFollow) {
+                    top.linkTo(rowStats.bottom, margin = 16.dp)
+                    start.linkTo(parent.start)
+                    end.linkTo(btnMsg.start)
+                    width = Dimension.wrapContent
                 }
-                Button(onClick = {}) {
-                    Text(text = "Direct Message")
+            ) {
+                Text(text = "Follow user")
+            }
+            Button(
+                onClick = {},
+                Modifier.constrainAs(btnMsg) {
+                    top.linkTo(rowStats.bottom, margin = 16.dp)
+                    start.linkTo(btnFollow.end)
+                    end.linkTo(parent.end)
+                    width = Dimension.wrapContent
                 }
+            ) {
+                Text(text = "Direct Message")
             }
         }
-    }
-}
-
-@Composable
-fun ProfileRow(content: @Composable RowScope.() -> Unit) {
-    Row(
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-    ) {
-        content()
     }
 }
 
