d1c619260f6c0353bded7d9d457ea5a8c49d8a7f
abGhost.cyber <xplendo@gmail.com>
2022-04-20 13:57:01 +0800

    jetpack compose basics workshop

--- app/src/main/java/com/crushtech/composelearning/MainActivity.kt
+++ app/src/main/java/com/crushtech/composelearning/MainActivity.kt
@@ -3,27 +3,22 @@ package com.crushtech.composelearning
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.crushtech.composelearning.ui.ProfilePage
 import com.crushtech.composelearning.ui.theme.ComposeLearningTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,117 +26,54 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeLearningTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    ProfilePage()
+                MyApp()
+            }
+        }
+    }
+
+    @Composable
+    fun MyApp() {
+        val (count, setCount) = remember { mutableStateOf(0) }
+        Surface(
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            color = Color(0xFF546E7A)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "$$count",
+                    style = TextStyle(
+                        fontSize = 35.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+                Spacer(modifier = Modifier.height(40.dp))
+                CreateCircle {
+                    setCount(count + 1)
                 }
             }
-
-//            var countrState by remember {
-//                mutableStateOf(0)
-//            }
-//            val painter = painterResource(id = R.drawable.kermit_the_frog)
-//            val description = "Kermit in the snow"
-//            val title = "Kermit is playing in the snow"
-
-//            Box(
-//                modifier = Modifier
-//                    .fillMaxWidth(0.5f)
-//                    .padding(16.dp),
-//            ) {
-//                ImageCard(
-//                    painter = painter,
-//                    contentDescription = description,
-//                    title = title
-//                )
-//            }
-
-//            Column(
-//                modifier = Modifier
-//                    .fillMaxHeight()
-//                    .padding(20.dp)
-//            ) {
-//                NamesList(modifier = Modifier.weight(1f), List(1000) { "Hello Android $it" })
-//                Counter(counter = counterState, updateCount = {
-//                    counterState = it
-//                })
-//            }
         }
     }
-}
 
-@Composable
-fun NamesList(
-    modifier: Modifier = Modifier,
-    names: List<String>,
-) {
-    LazyColumn(modifier = modifier) {
-        items(items = names) {
-            Greeting(name = it)
-            Divider()
-        }
-    }
-}
-
-@Composable
-fun Counter(counter: Int, updateCount: (Int) -> Unit) {
-    Button(onClick = { updateCount(counter + 1) }) {
-        Text(text = "I've been clicked $counter times")
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    var isSelected by remember {
-        mutableStateOf(false)
-    }
-    val targetColor by animateColorAsState(
-        targetValue = if (isSelected) MaterialTheme.colors.primary else Color.Transparent,
-        animationSpec = tween(durationMillis = 700)
-    )
-    Surface(color = targetColor) {
-        Text(
-            text = "Hello $name!",
+    @Composable
+    fun CreateCircle(onClick: () -> Unit) {
+        Card(
             modifier = Modifier
-                .clickable { isSelected = !isSelected }
-                .padding(16.dp)
-        )
-    }
-}
+                .padding(3.dp)
+                .size(105.dp)
+                .clickable {
+                    onClick.invoke()
+                },
+            shape = CircleShape, elevation = 5.dp
 
-@Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
-) {
-    Card(
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(15.dp),
-        elevation = 5.dp
-    ) {
-        Box(modifier = Modifier.height(200.dp)) {
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        Brush.verticalGradient(
-                            colors = listOf(Color.Transparent, Color.Black), startY = 300f
-                        )
-                    )
-            )
-            Text(
-                text = title,
-                modifier = modifier
-                    .align(Alignment.BottomStart)
-                    .padding(12.dp),
-                style = TextStyle(color = Color.White, fontSize = 16.sp)
-            )
+        ) {
+            Box(contentAlignment = Alignment.Center) {
+                Text(text = "Tap")
+            }
         }
     }
 }
--- app/src/main/java/com/crushtech/composelearning/ui/ProfilePage.kt
+++ app/src/main/java/com/crushtech/composelearning/ui/ProfilePage.kt
@@ -13,11 +13,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
 import com.crushtech.composelearning.R
 
@@ -30,78 +33,148 @@ fun ProfilePage() {
             .padding(top = 100.dp, start = 16.dp, end = 16.dp, bottom = 100.dp)
             .border(width = 2.dp, color = Color.White, shape = RoundedCornerShape(30.dp))
     ) {
-        ConstraintLayout {
-            val guideLine = createGuidelineFromTop(0.1f)
-            val (image, nameText, subText, rowStats, btnFollow, btnMsg) = createRefs()
-            Image(
-                painter = painterResource(id = R.drawable.kermit_the_frog),
-                contentDescription = "Kermit the frog",
-                modifier = Modifier
-                    .size(200.dp)
-                    .clip(CircleShape)
-                    .border(
-                        width = 3.dp,
-                        color = Color.Red, shape = CircleShape
-                    )
-                    .constrainAs(image) {
-                        top.linkTo(guideLine)
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                    },
-            )
-            Text(
-                text = "German Kermit", fontWeight = FontWeight.ExtraBold,
-                modifier = Modifier.constrainAs(nameText) {
-                    top.linkTo(image.bottom)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                }
-            )
-            Text(
-                text = "hello, welcome to my page",
-                modifier = Modifier.constrainAs(subText) {
-                    top.linkTo(nameText.bottom)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                }
-            )
-            Row(
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-                    .constrainAs(rowStats) {
-                        top.linkTo(subText.bottom)
-                    }
-            ) {
-                ProfileStats(count = 15000, desc = "Followers")
-                ProfileStats(count = 100, desc = "Following")
-                ProfileStats(count = 13, desc = "Post")
-                ProfileStats(count = 300, desc = "Saved")
+        BoxWithConstraints {
+            val constraints = if (minWidth < 600.dp) {
+                portraitConstraints(margin = 16.dp)
+            } else {
+                landscapeConstraints(margin = 16.dp)
             }
+            ConstraintLayout(constraintSet = constraints) {
+                Image(
+                    painter = painterResource(id = R.drawable.kermit_the_frog),
+                    contentDescription = "Kermit the frog",
+                    modifier = Modifier
+                        .size(200.dp)
+                        .clip(CircleShape)
+                        .border(
+                            width = 3.dp,
+                            color = Color.Red, shape = CircleShape
+                        )
+                        .layoutId("image"),
+                )
+                Text(
+                    text = "German Kermit", fontWeight = FontWeight.ExtraBold,
+                    modifier = Modifier.layoutId("nameText")
+                )
+                Text(
+                    text = "hello, welcome to my page",
+                    Modifier.layoutId("subText")
+                )
+                Row(
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                        .layoutId("rowStats")
+                ) {
+                    ProfileStats(count = 15000, desc = "Followers")
+                    ProfileStats(count = 100, desc = "Following")
+                    ProfileStats(count = 13, desc = "Post")
+                    ProfileStats(count = 300, desc = "Saved")
+                }
 
-            Button(
-                onClick = {},
-                modifier = Modifier.constrainAs(btnFollow) {
-                    top.linkTo(rowStats.bottom, margin = 16.dp)
-                    start.linkTo(parent.start)
-                    end.linkTo(btnMsg.start)
-                    width = Dimension.wrapContent
+                Button(
+                    onClick = {},
+                    Modifier.layoutId("btnFollow")
+                ) {
+                    Text(text = "Follow user")
                 }
-            ) {
-                Text(text = "Follow user")
-            }
-            Button(
-                onClick = {},
-                Modifier.constrainAs(btnMsg) {
-                    top.linkTo(rowStats.bottom, margin = 16.dp)
-                    start.linkTo(btnFollow.end)
-                    end.linkTo(parent.end)
-                    width = Dimension.wrapContent
+                Button(
+                    onClick = {},
+                    Modifier.layoutId("btnMsg")
+                ) {
+                    Text(text = "Direct Message")
                 }
-            ) {
-                Text(text = "Direct Message")
             }
+        }
+    }
+}
+
+private fun landscapeConstraints(margin: Dp): ConstraintSet {
+    return ConstraintSet {
+        val image = createRefFor("image")
+        val nameText = createRefFor("nameText")
+        val subText = createRefFor("subText")
+        val rowStats = createRefFor("rowStats")
+        val btnFollow = createRefFor("btnFollow")
+        val btnMsg = createRefFor("btnMsg")
+
+        constrain(image) {
+            top.linkTo(parent.top, margin)
+            start.linkTo(parent.start, margin)
+        }
+        constrain(nameText) {
+            start.linkTo(image.start)
+            top.linkTo(image.bottom)
+        }
+        constrain(subText) {
+            top.linkTo(nameText.bottom)
+            start.linkTo(nameText.start)
+            end.linkTo(nameText.end)
+        }
+        constrain(rowStats) {
+            top.linkTo(image.top)
+            start.linkTo(image.end, margin)
+            end.linkTo(parent.end)
+        }
+        constrain(btnFollow) {
+            top.linkTo(rowStats.bottom, margin = margin)
+            start.linkTo(rowStats.start)
+            end.linkTo(btnMsg.start)
+            bottom.linkTo(subText.bottom)
+            width = Dimension.wrapContent
+        }
+        constrain(btnMsg) {
+            top.linkTo(rowStats.bottom, margin = margin)
+            start.linkTo(btnFollow.end)
+            end.linkTo(parent.end)
+            bottom.linkTo(subText.bottom)
+            width = Dimension.wrapContent
+        }
+    }
+}
+
+private fun portraitConstraints(margin: Dp): ConstraintSet {
+    return ConstraintSet {
+        val image = createRefFor("image")
+        val guideLine = createGuidelineFromTop(0.1f)
+        val nameText = createRefFor("nameText")
+        val subText = createRefFor("subText")
+        val rowStats = createRefFor("rowStats")
+        val btnFollow = createRefFor("btnFollow")
+        val btnMsg = createRefFor("btnMsg")
+
+        constrain(image) {
+            top.linkTo(guideLine)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+        constrain(nameText) {
+            top.linkTo(image.bottom)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+        constrain(subText) {
+            top.linkTo(nameText.bottom)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+        constrain(rowStats) {
+            top.linkTo(nameText.bottom)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+        constrain(btnFollow) {
+            top.linkTo(rowStats.bottom, margin = margin)
+            start.linkTo(parent.start)
+            end.linkTo(btnMsg.start)
+            width = Dimension.wrapContent
+        }
+        constrain(btnMsg) {
+            top.linkTo(rowStats.bottom, margin = margin)
+            start.linkTo(btnFollow.end)
+            end.linkTo(parent.end)
+            width = Dimension.wrapContent
         }
     }
 }
