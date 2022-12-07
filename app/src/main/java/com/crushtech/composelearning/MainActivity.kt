d8575de0f5de49ed51632cf5020b6f524c71f101
AbGhost-cyber <xplendo@gmail.com>
2022-12-07 16:57:52 +0800

    ok

--- app/src/main/java/com/crushtech/composelearning/MainActivity.kt
+++ app/src/main/java/com/crushtech/composelearning/MainActivity.kt
@@ -3,10 +3,12 @@ package com.crushtech.composelearning
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -15,8 +17,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.crushtech.composelearning.ui.theme.ComposeLearningTheme
@@ -26,7 +33,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeLearningTheme {
-                MyApp()
+                Surface(color = MaterialTheme.colors.background) {
+                    MyBox()
+                }
             }
         }
     }
@@ -44,17 +53,17 @@ class MainActivity : ComponentActivity() {
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(
-                    text = "$$count",
-                    style = TextStyle(
-                        fontSize = 35.sp,
-                        fontWeight = FontWeight.Bold
-                    )
-                )
-                Spacer(modifier = Modifier.height(40.dp))
-                CreateCircle {
-                    setCount(count + 1)
-                }
+//                Text(
+//                    text = "$$count",
+//                    style = TextStyle(
+//                        fontSize = 35.sp,
+//                        fontWeight = FontWeight.Bold
+//                    )
+//                )
+//                Spacer(modifier = Modifier.height(40.dp))
+//                CreateCircle {
+//                    setCount(count + 1)
+//                }
             }
         }
     }
@@ -68,12 +77,78 @@ class MainActivity : ComponentActivity() {
                 .clickable {
                     onClick.invoke()
                 },
-            shape = CircleShape, elevation = 5.dp
+            shape = CircleShape,
+            elevation = 5.dp
 
         ) {
             Box(contentAlignment = Alignment.Center) {
                 Text(text = "Tap")
             }
         }
+    }
+
+    @Composable
+    fun RowScope.CustomItem(weight: Float, color: Color) {
+        Surface(
+            modifier = Modifier
+                .width(50.dp)
+                .height(50.dp)
+                .padding(start = 5.dp, end = 5.dp)
+                .weight(weight),
+            color = color,
+            elevation = 5.dp
+        ) {}
+    }
+
+    @Composable
+    fun CreateRectangle() {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            CustomItem(1f, MaterialTheme.colors.secondary)
+            CustomItem(1f, MaterialTheme.colors.primary)
+        }
+    }
+
+    @Composable
+    fun MyBox() {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Text(
+                text = stringResource(id = R.string.app_name),
+                modifier = Modifier
+                    .background(MaterialTheme.colors.primary)
+                    .padding(16.dp),
+                color = Color.White,
+                fontSize = MaterialTheme.typography.h5.fontSize,
+                fontWeight = FontWeight.SemiBold,
+                textAlign = TextAlign.End
+            )
+            CustomText()
+        }
+    }
+
+    @Composable
+    fun CustomText() {
+        Text(
+            buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Red,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 40.sp
+                    )
+                ) {
+                    append("A")
+                }
+                withStyle(style = ParagraphStyle()) {
+                }
+                append("B")
+                append("C")
+                append("D")
+            },
+            modifier = Modifier.width(200.dp)
+        )
     }
 }
